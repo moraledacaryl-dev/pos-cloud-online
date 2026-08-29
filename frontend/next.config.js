8fc9040dd133eb9936c78a4c130c5847817a7ec6
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-08-29 19:18:26 +0800

    Pass 15: remove unsafe-eval from production CSP

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,7 +10,7 @@ const contentSecurityPolicy = [
   "img-src 'self' data: blob:",
   "font-src 'self' data:",
   "style-src 'self' 'unsafe-inline'",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+  "script-src 'self' 'unsafe-inline'",
   "connect-src 'self'",
   "media-src 'self'",
 ].join('; ');
