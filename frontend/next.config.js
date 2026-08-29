43c28f57b793545047e42b4062b23d707386adb5
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-08-29 17:43:35 +0800

    Pass 12: allow same-origin KDS media only

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,6 +12,7 @@ const contentSecurityPolicy = [
   "style-src 'self' 'unsafe-inline'",
   "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
   "connect-src 'self'",
+  "media-src 'self'",
 ].join('; ');
 
 const securityHeaders = [
